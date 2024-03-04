eb117de55dd8dc65f41db563c5332562bc350bfa
carolynqu <67798872+carolynqu@users.noreply.github.com>
2024-03-04 15:53:44 -0500

    buttons

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,5 @@
-import {View, FlatList} from 'react-native';
+import { Redirect } from "expo-router";
 
-import products from '@assets/data/products';
-import ProductListItem from '@components/ProductListItem';
-
-export default function TabOneScreen() {
-  return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({item}) => <ProductListItem product={item} />}
-        numColumns={2}
-        contentContainerStyle = {{gap: 10, padding: 10}}
-        columnWrapperStyle = {{gap: 10}}
-      />
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={"/menu/"} />;
 }
