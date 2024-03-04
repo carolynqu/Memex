94a118ef9d7aa7fcf847b5424d78a99bcfe22ffb
carolynqu <67798872+carolynqu@users.noreply.github.com>
2024-03-04 12:24:37 -0500

    route setting

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,50 +1,56 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 
-import Colors from '../constants/Colors';
-import { Product } from '../types';
+import Colors from "../constants/Colors";
+import { Product } from "../types";
+import { Link } from "expo-router";
 
-export const defaultPizzaImage =  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
-    product: Product;
+  product: Product;
 };
 
-const ProductListItem = ({product}:ProductListItemProps) => {
-	return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image || defaultPizzaImage}}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-    </View>
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+      </Pressable>
+    </Link>
   );
-}
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
- container: {
-    backgroundColor: 'white',
+  container: {
+    backgroundColor: "white",
     borderRadius: 20,
     padding: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 1,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   title: {
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 18,
     marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
-    fontWeight: 'bold',
-    marginTop: 'auto',
+    fontWeight: "bold",
+    marginTop: "auto",
   },
 });
