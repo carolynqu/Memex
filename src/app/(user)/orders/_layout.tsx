3c99e2dbad4e191b432f604c3b451b029ddf1e05
carolynqu <67798872+carolynqu@users.noreply.github.com>
2024-03-06 15:01:22 -0500

    frontend finished

---
+++ src/app/(user)/orders/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+export default function MenuStack() {
+  return (
+    <Stack screenOptions={{}}>
+      <Stack.Screen name="index" options={{ title: "Orders" }} />
+    </Stack>
+  );
+}
